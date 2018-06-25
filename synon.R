72dea65d0ca1030b4a52ece48338574a5ac0e3d3
mwalte10 <mwalte10@nd.edu>
2018-06-25 17:58:05 -0400

    started a fake way to find number of similar sequences

--- synon.R
+++ synon.R
@@ -5,3 +5,12 @@ setwd('~/Desktop/CSHL/')
 load('id.RData')
 load('DNA.RData')
 load('AA.RData')
+
+unique_dna <- unique(dna_temp)
+unique_aa <- unique(aa_temp)
+
+##this isn't working but need to do something like this
+counts_dna <- c()
+for(i in 1:length(unique_dna)){
+  counts_dna[i] <- length(which(dna_temp == unique_dna[i]))
+}
