c1b827d837f4642833db72a6f576b978e50ed70d
mwalte10 <mwalte10@nd.edu>
2018-06-26 09:54:44 -0400

    cleaned up DNA, AA, ID, and dates so that they are all the same length. Also added some checks in so that the index can be identified which is causing problems/offsetting things

--- data_clean.R
+++ data_clean.R
@@ -10,6 +10,7 @@ dna_2017 <- read.fasta('dna_2017.fasta')
 dna <- c(dna_2012_2015, dna_2016, dna_2017)
 dna_temp <- list()
 dna_id <- c()
+date <- c()
 for(i in 1:length(dna)){
   x <- unlist(dna[[i]])
   dna_temp[[i]] <- paste(x, collapse = '')
@@ -17,17 +18,33 @@ for(i in 1:length(dna)){
   dna_temp[[i]] <- paste(x[beg:length(x)], sep = '')
   a <- unlist(strsplit(unlist(getAnnot(x)), ' '))
   dna_id[i] <- a[length(a)]
+  date[i] <- a[1]
 }
-unique_dna_id <- unique(dna_id)
 
-#remove any sequences with the same ID
-unique_dna <- dna_id[-which(duplicated(dna_id) == TRUE)]
+#clean date
+date_temp <- c()
+for(i in 1:length(date)){
+  date_temp[i] <- unlist(strsplit(unlist(date[i]), '>'))[2]
+}
+date <- date_temp
+date <- as.Date(date)
 
-#total number of nucleotide sequences is 39572
-dna_temp <- dna_temp[-which(duplicated(dna_id) == TRUE)]
-#total number of unique nucleotide sequences 22105
-unique_dna <- unique(dna_temp)
-length(unique_dna)
+#remove duplicates and those w/o dates
+duplicates <- which(duplicated(dna_id) == TRUE)
+no_dates <- which(is.na(date) == TRUE)
+remove_dna <- c(duplicates, no_dates)
+
+#total number of nucleotide sequences is 39304
+date <- date[-remove_dna]
+date <- date[-16417]
+dna_temp <- dna_temp[-remove_dna]
+dna_temp <- dna_temp[-16417]
+dna_id <- dna_id[-remove_dna]
+dna_id <- dna_id[-16417]
+
+
+#total number of unique nucleotide sequences 21937
+length(unique(dna_temp))
 
 
 aa_2012_2015 <- read.fasta('aa_2012_2015.fasta')
@@ -37,6 +54,7 @@ aa_2017 <- read.fasta('aa_2017.fasta')
 aa <- c(aa_2012_2015, aa_2016, aa_2017)
 aa_temp <- list()
 aa_id <- c()
+aa_date <- c()
 for(i in 1:length(aa)){
   tryCatch({
     x <- unlist(aa[[i]])
@@ -44,17 +62,28 @@ for(i in 1:length(aa)){
     beg <- str_locate(aa_temp[[i]], "m")[1]
     aa_temp[[i]] <- paste(x[beg:length(x)], sep = '')
     aa_id[i] <- attr(x,"name")
+    a <- unlist(strsplit(unlist(getAnnot(x)), ' '))
+    aa_date[i] <- a[length(a)] 
   }, error=function(e){cat("ERROR :",conditionMessage(e), "\n")})
 }
 
-#remove any sequences with the same ID
-unique_aa_id <- aa_id[-which(duplicated(aa_id) == TRUE)]
-#total number of aa sequences is 39572
-aa_temp <- aa_temp[-which(duplicated(aa_id) == TRUE)]
-length(aa)
-#total number of unique amino acid sequences is 10170
-unique_aa <- unique(aa)
-length(unique_aa)
+
+#remove duplicates and those w/o dates
+aa_date <- as.Date(aa_date)
+unknown_date <- which(is.na(aa_date) == TRUE)
+duplicate_id <- which(duplicated(aa_id) == TRUE)
+
+remove_aa <- c(unknown_date, duplicate_id)
+
+#total number of aa sequences is 39304
+aa_temp <- aa_temp[-remove_aa]
+aa_date <- aa_date[-remove_aa]
+aa_id <- aa_id[-remove_aa]
+
+#total number of unique amino acid sequences is 10105
+length(unique(aa_temp))
+
+unique_aa_id <- unique(aa_id)
 
 id <- unique_aa_id
 
@@ -64,3 +93,19 @@ write.fasta(aa, unique_aa_id,'AA.fasta')
 save(id, file = 'id.RData')
 save(dna_temp, file = 'DNA.RData')
 save(aa_temp, file = 'AA.RData')
+save(date, file = 'date.RData')
+
+#CHECK TO ENSURE THAT AAs and DNA are the same
+# for(i in 1:length(date)){
+#   if(date[i] != aa_date[i]){print(i) & break}
+# }
+
+# for(i in 1:length(aa_id)){
+#   if(aa_id[i] != dna_id[i]){print(i) & break}
+# }
+
+
+
+
+
+
