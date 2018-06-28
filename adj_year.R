7b734925ccef6974e69f5dd70c38e7259b1c27b0
mwalte10 <mwalte10@nd.edu>
2018-06-28 11:38:08 -0400

    cleaned up matrix, converting to sparse at the end instead of the beginning in order to make it more workable. Also starting to save the network data ('network_years.RData')

--- adj_year.R
+++ adj_year.R
@@ -8,7 +8,8 @@ load('UNIQUE_DNA.RData')
 library(Matrix)
 dna_year <- dna[(which(dna[,1] == years)),]
 
-assign(paste0('adj_mat_', years, sep = ''), Matrix(NA, nrow = dim(dna_year)[1], ncol = dim(dna_year)[1], sparse = TRUE)) 
+#assign(paste0('adj_mat_', years, sep = ''), matrix(0, nrow = dim(dna_year)[1], ncol = dim(dna_year)[1])) 
+mat <- matrix(0, nrow = dim(dna_year)[1], ncol = dim(dna_year)[1])
 
 for(i in 1:dim(dna_year)[1]){
   comp <- dna_year[,5][i]
@@ -17,11 +18,28 @@ for(i in 1:dim(dna_year)[1]){
     to <- dna_year[,5][j]
     to <- unlist(to)
     x <- length(which(comp != to))
-    mat <- get(paste('adj_mat_', years, sep = ''))
-    if(x == 1){mat[i,j] <- 1} 
+    ifelse(x == 1, mat[i,j] <-  1, 
+           mat[i,j] <-  0)
   }
   print(i / dim(dna_year)[1])
 }
 
-y <- get(paste('adj_mat_', years, sep = ''))
-save(y, file = paste('adj_mat_', years, '.RData', sep = ''))
+colnames(mat) <- unlist(dna_year[,4])
+rownames(mat) <- unlist(dna_year[,4])
+
+
+counts <- colSums(mat)
+names(counts) <- unlist(dna_year[,4])
+
+###Essentially gets the coordinates of the ones which are interacting
+cols <- (which(mat==1, arr.ind=TRUE))[,2]
+rows <- (which(mat==1, arr.ind=TRUE))[,1]
+new <- cbind(unlist(dna_year[,4])[cols], unlist(dna_year[,4])[rows])
+test <- network(new, matrix.type = 'edgelist', directed = FALSE)
+plot.network(test, main = "2013 DNA Sequence Network \nAll Unique Sequences", vertex.col = 'grey', 
+             usecurve = 0, #vertex.cex = seq(0.2, 1.3, length.out = max(counts))
+             jitter= FALSE)
+
+sparse_mat <- as(mat, "sparseMatrix") 
+save(test, file = paste('network_', years, '.RData'))
+save(sparse_mat, file = paste('adj_mat_', years, '.RData'))
