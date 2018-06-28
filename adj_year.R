bc94cf398823f7b9c7e59782e6ba86d5cc8dc9c1
mwalte10 <mwalte10@nd.edu>
2018-06-28 09:47:26 -0400

    changed paste/ assign arguments

--- adj_year.R
+++ adj_year.R
@@ -3,11 +3,12 @@ input = as.numeric(args[1])
 years <- c(2012, 2013, 2014, 2015, 2016, 2017)
 years <- years[input]
 
+#setwd('~/Desktop/CSHL/')
 load('UNIQUE_DNA.RData')
 library(Matrix)
 dna_year <- dna[(which(dna[,1] == years)),]
 
-paste('adj_mat_', years, sep = '') <- Matrix(NA, nrow = dim(dna_year)[1], ncol = dim(dna_year)[1], sparse = TRUE)
+assign(paste0('adj_mat_', years, sep = ''), Matrix(NA, nrow = dim(dna_year)[1], ncol = dim(dna_year)[1], sparse = TRUE)) 
 
 for(i in 1:dim(dna_year)[1]){
   comp <- dna_year[,5][i]
@@ -16,7 +17,8 @@ for(i in 1:dim(dna_year)[1]){
     to <- dna_year[,5][j]
     to <- unlist(to)
     x <- length(which(comp != to))
-    if(x == 1){adj_mat_year[i,j] <- 1} 
+    mat <- get(paste('adj_mat_', years, sep = ''))
+    if(x == 1){mat[i,j] <- 1} 
   }
   print(i / dim(dna_year)[1])
 }
